cfc2f2e4a69e212c035424d2c66b460ee7a89749
Vishal Singh <vs6073255@gmail.com>
2025-09-03 15:52:35 +0530

    add some feature on login page

--- core/views.py
+++ core/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, redirect
-from .forms import LoginForm 
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 def login_view(request):
-    
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login_success')
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
 
+            if user is not None:
+                login(request, user)
+                if user.groups.filter(name='super_admin').exists():
+                    return redirect('dashboard')
+                elif user.groups.filter(name='agent').exists():
+                    return redirect('agent')
+                else:
+                    return redirect('client')
+            else:
+
+                form.add_error(None, "Invalid email or password.")
     else:
-        
         form = LoginForm()
 
-    
-
     return render(request, 'core/login.html', {'form': form})
-
-def login_success_view(request):
-    return render(request, 'core/login_success.html')
-
