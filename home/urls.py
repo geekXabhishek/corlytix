d437e70e68befe8a1857e248dbed3ecb5732b282
ashish666-hub <ashish1800063@gmail.com>
2025-09-10 16:54:52 +0530

    home page added

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ] 
